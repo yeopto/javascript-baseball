6243054dabdb893e02c4615d32ab819ca32a9fd6
yeopto <gunyeop530@gmail.com>
2022-11-07 17:33:56 +0900

    test: 임의의 수와 사용자 입력 수를 비교하여 결과를 출력하는 기능 테스트 코드 작성

--- __tests__/MyfunctionTest.js
+++ __tests__/MyfunctionTest.js
@@ -1,4 +1,5 @@
 const { isValidNum } = require('../src/modules/isValidNum');
+const { compareInputNum } = require('../src/modules/compareInputNum');
 
 describe("기능 TEST", () => {
   test('사용자 입력 숫자 유효한지 판단하는 기능', () =>  {
@@ -6,5 +7,12 @@ describe("기능 TEST", () => {
     const correctAnswer = input.filter((el) => isValidNum(el));
 
     expect(correctAnswer).toEqual(['234']);
+  });
+  test('임의의 수와 사용자 입력 수를 비교하여 결과를 출력하는 기능', () =>  {
+    const computerNums = '245';
+    const inputNum = ['234', '354', '678'];
+    const result = inputNum.map((el) => compareInputNum(computerNums, el));
+
+    expect(result).toEqual(['1볼 1스트라이크', '2볼', '낫싱']);
   });
 });
