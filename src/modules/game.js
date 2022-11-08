86c57e5d2635fd2eec94f298081c33796296f633
yeopto <gunyeop530@gmail.com>
2022-11-08 19:40:57 +0900

    refactor: baseBallGame 함수에서 재시작 또는 종료하는 로직 gameReStartOrExit 함수로 분리

--- src/modules/game.js
+++ src/modules/game.js
@@ -4,6 +4,15 @@ const { isValidNum } = require('./isValidNum');
 const { compareInputNum } = require('./compareInputNum');
 const { generateRandomNum } = require('./generateRandomNum');
 
+function gameReStartOrExit() {
+  MissionUtils.Console.readLine(GAME_MESSAGE.RESTART_OR_END_MESSAGE, (userInputNums) => {
+    if (!(userInputNums === '1' || userInputNums === '2')) throw GAME_MESSAGE.ERROR_THROW_MESSAGE;
+    if (userInputNums === '1') return baseBallGame(generateRandomNum());
+    MissionUtils.Console.close();
+  });
+  return;
+}
+
 function gameFirstStart() {
   return MissionUtils.Console.print(GAME_MESSAGE.GAME_START_MESSAGE);
 }
@@ -16,11 +25,7 @@ function baseBallGame(computerNums) {
     MissionUtils.Console.print(compareInputNum(computerNums, userInputNums));
     if (computerNums !== userInputNums) return baseBallGame(computerNums);
     MissionUtils.Console.print(GAME_MESSAGE.GAME_END_MESSAGE);
-    MissionUtils.Console.readLine(GAME_MESSAGE.RESTART_OR_END_MESSAGE, (userInputNums) => {
-      if (!(userInputNums === '1' || userInputNums === '2')) throw GAME_MESSAGE.ERROR_THROW_MESSAGE;
-      if (userInputNums === '1') return baseBallGame(generateRandomNum());
-      MissionUtils.Console.close();
-    });
+    gameReStartOrExit();
   });
   return;
 }
